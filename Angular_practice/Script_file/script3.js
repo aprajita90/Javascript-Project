454eee89ddf2b650291a7968cba966656c42b39d
aprajita_jain <aprajitajain90@gmail.com>
2017-11-11 15:01:20 -0800

    Search filter in AngularJs

--- Angular_practice/Script_file/script3.js
+++ Angular_practice/Script_file/script3.js
@@ -7,11 +7,11 @@ var myApp = angular
 			.module("myModule", [])
 			.controller("myController", function ($scope){
 				var employees = [
-				{name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender:"Male", salary: 55000.788},
-				{name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender:"Female", salary: 68000},
-				{name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender:"Male", salary: 57000},
-				{name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender:"Female", salary: 53000},
-				{name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender:"Male", salary: 50000}
+				{name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender:"Male", salary: 55000.788, city:"London"},
+				{name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender:"Female", salary: 68000, city:"Chennai"},
+				{name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender:"Male", salary: 57000, city:"London"},
+				{name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender:"Female", salary: 53000, city:"Chennai"},
+				{name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender:"Male", salary: 50000, city:"London"}
 
 				];
 				$scope.employees = employees;
@@ -19,7 +19,7 @@ var myApp = angular
 				$scope.sortColumn=name;
 				$scope.reverseSort = false;
 
-				//bidirectional sort in angular js
+				//bidirectional sort in angularJs
 
 				$scope.sortData = function(column){
 					$scope.reverseSort = ($scope.sortColumn === column)? !$scope.reverseSort: false;
